62db0cd0e535fbb52facf30a9b7ba481a39d5120
Malherbe <malherbe.billy@gmail.com>
2021-03-25 23:31:31 +0800

    Solution for double_trans_cipher

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,3 +1,11 @@
+class Array
+  def unshuffle(random:)
+    matrix_order = (0...length).to_a.shuffle!(random: random)
+    sort_by.with_index{|_, k| matrix_order[k]}
+  end
+end
+
+
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
@@ -7,12 +15,18 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
-    
-    
+    array_matrix = document.split("").each_slice(key).to_a
+    row_shuffle = array_matrix.shuffle(random: Random.new(key))
+    joined_string = row_shuffle.join('')
+    return joined_string
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    array_matrix_decry = document.split("").each_slice(key).to_a
+    row_shuffle_decry = array_matrix_decry.unshuffle(random: Random.new(key))
+    joined_string_decry = row_shuffle_decry.join('')
+    return joined_string_decry
   end
 end
 
